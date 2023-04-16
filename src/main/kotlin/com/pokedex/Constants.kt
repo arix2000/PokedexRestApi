ac46559595da158744756860356955b412715195
Arek Mądry <arkadiusz.madry.2000.com>
2023-04-16 21:18:07 +0200

    refactor

--- src/main/kotlin/com/pokedex/Constants.kt
+++ src/main/kotlin/com/pokedex/Constants.kt
@@ -1,5 +1,5 @@
 package com.pokedex
 
 object Constants {
-    const val BASE_URL = ""
+    const val BASE_URL = "http://192.168.1.21:8080"
 }
--- src/main/kotlin/com/pokedex/routes/PokemonListRoute.kt
+++ src/main/kotlin/com/pokedex/routes/PokemonListRoute.kt
@@ -2,8 +2,6 @@ package com.pokedex.routes
 
 import io.ktor.server.routing.*
 
-private const val BASE_URL = "http://192.168.1.21:8080"
-
 fun Route.pokemonList() {
     get("/pokemonList") {
 
